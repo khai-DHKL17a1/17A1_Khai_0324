5514c4c9087b4cb6c743f0e3b9e8d034f851b08d
khai-DHKL17a1 <149310935+khai-DHKL17a1@users.noreply.github.com>
2023-11-10 21:48:43 +0700

    baitapchuong7.py

--- baitapchuong7.py
+++ baitapchuong7.py
@@ -1,42 +1,42 @@
 Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> #bài 7.1
->>> x = 6
->>> s = 1 + x + x**3/3 + x**5/5
->>> print(s)
+>>> #bài 7.1 
+x = 6
+s = 1 + x + x**3/3 + x**5/5
+print(s)
 1634.2
 >>> #bài 7.2
->>> result = 1+2
->>> print('result',result)
+result = 1+2
+print('result',result)
 result 3
->>> original_result= result
->>> result = result + 1
->>> print('result',result)
+original_result= result
+result = result + 1
+print('result',result)
 result 4
->>> original_result= result
->>> result = result * 2
->>> original_result= result
->>> print('result',result)
+original_result= result
+result = result * 2
+original_result= result
+print('result',result)
 result 8
->>> result = result ** 3
->>> original_result= result
->>> print('result',result)
+result = result ** 3
+original_result= result
+print('result',result)
 result 512
->>> result = result + 8
->>> original_result= result
->>> print('result',result)
+result = result + 8
+original_result= result
+print('result',result)
 result 520
->>> result = result % 7
->>> original_result= result
->>> print('result',result)
+result = result % 7
+original_result= result
+print('result',result)
 result 2
->>> result = result // 2
->>> original_result= result
->>> print('result',result)
+result = result // 2
+original_result= result
+print('result',result)
 result 1
->>> #bài 7.3
->>> result= 5
->>> print('result',result)
+#bài 7.3
+result= 5
+print('result',result)
 result 5
 result -= 1
 print('result',result)
@@ -105,3 +105,20 @@ print('x is y:',x is y)
 x is y: False
 print('x is not y:',x is not y)
 x is not y: True
+# bài 7.7
+đổi tiền
+so_tien_muon_doi = float(input("Số tiền muốn đổi :"))
+so_to_1 = so_tien_muon_doi // 500000
+tien_con_lai = so_tien_muon_doi%500000
+print('Số tờ 500,000 :', so_to_1)
+so_to_2 = tien_con_lai//200000
+tien_con_lai = tien_con_lai%200000
+print('Số tờ 200,000 :', so_to_2)
+so_to_3 = tien_con_lai//100000
+tien_con_lai = tien_con_lai%100000
+print('Số tờ 100,000 :',so_to_3)
+so_to_4 = tien_con_lai//50000
+tien_con_lai = tien_con_lai%50000
+print('Số tờ 50,000 :', so_to_4)
+tien_con_lai = tien_con_lai*1
+print('Tiền còn lại :', tien_con_lai)
